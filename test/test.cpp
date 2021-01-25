588df893b9779cd94101c8a19b742f64400099ca
Hamish Pierpont <hamish.pierpont@ufl.edu>
2021-01-25 13:05:02 -0500

    Updated test.cpp for clarity

--- test/test.cpp
+++ test/test.cpp
@@ -4,20 +4,20 @@
 #include "catch.hpp"
 
 TEST_CASE("Vector sum is computed correctly", "[vector]"){
-    std::vector<int> sum1 = {1, 2, 3};
-    REQUIRE(sumVector(sum1) == 6);
-    REQUIRE_FALSE(sumVector(sum1) != 6);
+    std::vector<int> vec = {1, 2, 3};
+    REQUIRE(sumVector(vec) == 6);
+    REQUIRE_FALSE(sumVector(vec) != 6);
 }
 
-TEST_CASE("This should fail", "[vector][fails_on_purpose]") {
-    std::vector<int> sum2 = {1, 1, 1};
-    REQUIRE(sumVector(sum2) == 75); //Inside part evaluates to false, so this test will fail
+TEST_CASE("This fails", "[vector]") {
+    std::vector<int> vec = {1, 2, 3};
+    REQUIRE(sumVector(vec) == 75); //Inside part evaluates to false, so this test will fail
 }
 
-TEST_CASE("Vector sum modulo n should be computed correctly", "[vector][modulo]") {
-    std::vector<int> sum3 = {1, 2, 3};
-    REQUIRE(sumVectorModN(sum3, 5) == 1);
-    REQUIRE(sumVectorModN(sum3, 4) == 2);
-    REQUIRE_FALSE(sumVectorModN(sum3, 1) == 1); //Any number mod 1 is 0
-    REQUIRE_THROWS(sumVectorModN(sum3, -1)); //Should throw a logic_error, so test should pass
+TEST_CASE("Vector sum modulo n is computed correctly", "[vector][modulo]") {
+    std::vector<int> vec = {1, 2, 3};
+    REQUIRE(sumVectorModN(vec, 5) == 1);
+    REQUIRE(sumVectorModN(vec, 4) == 2);
+    REQUIRE_FALSE(sumVectorModN(vec, 1) == 1); //Any number mod 1 is 0, expression is false, so assertion passes
+    REQUIRE_THROWS(sumVectorModN(vec, -1)); //Should throw a logic_error, so test should pass
 }
